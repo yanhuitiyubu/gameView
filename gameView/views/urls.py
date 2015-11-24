dad18a99a85aeae793ee930c4ac55aefc347bac4
yanhuitiyubu <317551593@qq.com>
2015-11-24 17:45:06 +0800

    Make all pages work well except one_team and one_member

--- gameView/views/urls.py
+++ gameView/views/urls.py
@@ -8,4 +8,10 @@ import views
 urlpatterns = patterns('',
     #url(r'^index$', views.index, name='index'),
     url(r'^$', views.index, name='index'),
+    url(r'^news$', views.news, name='news'),
+    url(r'^schedule', views.schedule, name='schedule'),
+    url(r'^basketball_info', views.basketball_info, name='basketball_info'),
+    url(r'^football_info', views.basketball_info, name='football_info'),
+    url(r'^football_rank', views.basketball_info, name='football_rank'),
+    url(r'^basketball_rank', views.basketball_info, name='basketball_rank'),
 )
--- gameView/views/views.py
+++ gameView/views/views.py
@@ -19,3 +19,81 @@ def index(request):
         args_data,
         context_instance=RequestContext(request)
     )
+
+def news(request):
+    """
+    Home page of gameview
+    """
+    args_data = {
+    'cv_news' : True,
+    }
+    return render_to_response(
+        "views/news.html",
+        args_data,
+        context_instance=RequestContext(request)
+    )
+
+def schedule(request):
+    """
+    Home page of gameview
+    """
+    args_data = {
+    'cv_schedule' : True,
+    }
+    return render_to_response(
+        "views/schedule.html",
+        args_data,
+        context_instance=RequestContext(request)
+    )
+
+def basketball_info(request):
+    """
+    Home page of gameview
+    """
+    args_data = {
+    'cv_basketball_info' : True,
+    }
+    return render_to_response(
+        "views/teamlist_bas.html",
+        args_data,
+        context_instance=RequestContext(request)
+    )
+
+def football_info(request):
+    """
+    Home page of gameview
+    """
+    args_data = {
+    'cv_football_info' : True,
+    }
+    return render_to_response(
+        "views/teamlist_soc.html",
+        args_data,
+        context_instance=RequestContext(request)
+    )
+
+def basketball_rank(request):
+    """
+    Home page of gameview
+    """
+    args_data = {
+    'cv_basketball_rank' : True,
+    }
+    return render_to_response(
+        "views/rank_bas.html",
+        args_data,
+        context_instance=RequestContext(request)
+    )
+
+def football_rank(request):
+    """
+    Home page of gameview
+    """
+    args_data = {
+    'cv_football_rank' : True,
+    }
+    return render_to_response(
+        "views/rank_soc.html",
+        args_data,
+        context_instance=RequestContext(request)
+    )
